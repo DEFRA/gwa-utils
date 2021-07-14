95065408d8ef77fd493cfada44cc33e477b4b2ee
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-07-14 11:16:44 +0100

    feat: update test file for new message format, use UK mobile

--- generate-initial-file.js
+++ generate-initial-file.js
@@ -8,17 +8,20 @@ console.log(`Generating a list of ${numberOfContacts} contacts`)
 const contacts = []
 
 // '07700900000' is a recognised test phoneNumber (for Notify)
-let numberBase = 100000000
+let numberBase = 10000000
 console.time('gen-contacts')
 for (let i = 0; i < numberOfContacts; i++) {
-  contacts.push({ phoneNumber: `07${numberBase++}` })
+  contacts.push({ phoneNumber: `077${numberBase++}` })
 }
 console.timeEnd('gen-contacts')
 
-const message = 'This is a test message. Please ignore. There is nothing to see here. This is the way. It has a length so as not to exceed a single message i.e. 160 chars.'
+const text = 'This is a test message. It is being sent via Notify using an API key of type TEST.'
 
 const msg = {
-  message,
+  message: {
+    id: `msg-id-${listSize}`,
+    text
+  },
   contacts
 }
 
